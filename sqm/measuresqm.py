efb5ca930676bc33b6bdee953fd69ab9d5b5e09f
starburstgalaxy00 <starburstgalaxy00@gmail.com>
2025-08-10 13:55:51 +0900

    update sqm

--- sqm/measuresqm.py
+++ sqm/measuresqm.py
@@ -1,8 +1,9 @@
 import socket
 import time
 import csv
-from astral.sun import sun
+from astral.sun import sun, dusk, dawn
 from astral import LocationInfo
+from astral import moon
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
@@ -12,18 +13,23 @@ def log(message):
     with open("SQMlog.txt","a",encoding="utf-8") as f:
         f.write(f"{timestamp} | {message}\n")
 
-# 일몰, 일출 시간 불러오기
+# 밤하늘 정보 불러오기
 def nighttime(city,country,timezone,latitude,longitude):
     location=LocationInfo(city,country,timezone,latitude=latitude, longitude=longitude)
     now=datetime.now(ZoneInfo(location.timezone))
 
-    sun_today=sun(location.observer,date=now.date(),tzinfo=location.timezone)
-    sun_tomorrow=sun(location.observer,date=now.date()+timedelta(days=1),tzinfo=location.timezone)
+    sun_today=sun(location.observer, date=now.date(), tzinfo=location.timezone)
+    sun_tomorrow=sun(location.observer, date=now.date()+timedelta(days=1), tzinfo=location.timezone)
 
     sunset=sun_today["sunset"]
     sunrise=sun_tomorrow["sunrise"]
 
-    return now, sunset, sunrise
+    dusk_today=dusk(location.observer, date=now.date(), tzinfo=location.timezone, depression=18)
+    dawn_tomorrow=dawn(location.observer, date=now.date(), tzinfo=location.timezone, depression=18)
+
+    moon_phase=moon.phase(now)
+
+    return now, sunset, sunrise, dusk_today, dawn_tomorrow, sun_today, sun_tomorrow, moon_phase, location.timezone
 
 # 시리얼번호 가져오기
 def serialnumber(IP,PORT):
@@ -46,15 +52,15 @@ def serialnumber(IP,PORT):
             return "unknown"
 
 # 데이터 평균값값 저장하기
-def average(avg,city,serial):
+def average(avg_sun,avg_twilight,moon_phase,city,serial):
     today = datetime.now().strftime("%Y-%m-%d")
     filename=f"sqmavg_{city}_{serial}.csv"
     with open(filename,"a",newline="",encoding="utf-8") as f:
         writer=csv.writer(f)
-        writer.writerow([today,avg])
+        writer.writerow([today,avg_sun,avg_twilight,moon_phase])
 
 # 데이터 일일값 저장하기
-def daily(brightness_values, temperature_values, photon_values, timestamps, sunset, city, serial):
+def daily(brightness_values, temperature_values, photon_values, timestamps, sunset, city, serial, moon_phase, location_timezone):
     sunset_date = sunset.date().strftime("%Y-%m-%d")
     filename=f"{sunset_date}_{city}_{serial}.csv"
 
@@ -65,7 +71,7 @@ def daily(brightness_values, temperature_values, photon_values, timestamps, suns
             writer.writerow([ts,bri, temp, pho])
 
 # 데이터 측정하기
-def measurement(IP,PORT,timeinterval,sunset,now,sunrise,city,serial):
+def measurement(IP,PORT,timeinterval,sunset,now,sunrise,dusk_today,dawn_tomorrow,city,serial,moon_phase, location_timezone):
     timeduration=sunrise-now
     # n=10
     n=int(timeduration.total_seconds()//timeinterval)
@@ -74,7 +80,6 @@ def measurement(IP,PORT,timeinterval,sunset,now,sunrise,city,serial):
     temperature_values=[]
     photon_values=[]
     timestamps=[]
-    
     for i in range(n):
         timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         try:
@@ -126,8 +131,19 @@ def measurement(IP,PORT,timeinterval,sunset,now,sunrise,city,serial):
 
     if brightness_values:
         avg_brightness=sum(brightness_values)/len(brightness_values)
-        average(avg_brightness,city,serial)
-        daily(brightness_values,temperature_values,photon_values,timestamps,sunset,city,serial)
+        twilight_brightnesses=[
+            bri for bri, ts in zip(brightness_values,timestamps)
+            if dusk_today <= datetime.strptime(ts, "%Y-%m-%d %H:%M:%S").replace(tzinfo=location_timezone) <= dawn_tomorrow
+        ]
+
+        if twilight_brightnesses:
+            avg_twilight=sum(twilight_brightnesses)/len(twilight_brightnesses)
+        else:
+            avg_twilight=None
+            log("WARNING: No data in astronomical twilight range.")
+            
+        average(avg_brightness,avg_twilight,moon_phase,city,serial)
+        daily(brightness_values,temperature_values,photon_values,timestamps,sunset,city,serial,moon_phase,location_timezone)
         print(f"Average brightness saved: {avg_brightness}")
         log(f"average brightness saved: {avg_brightness}")
         print(f"Daily data saved")
@@ -143,19 +159,19 @@ timezone="Asia/Seoul"
 latitude=36.3667
 longitude=127.3556
 
-IP='192.168.0.28'
+IP='192.168.0.7'
 PORT=10001
-timeinterval=1
+timeinterval=30
 
 serial=serialnumber(IP,PORT)
 
 while True:
-    now, sunset, sunrise = nighttime(city,country,timezone,latitude,longitude)
+    now, sunset, sunrise, dusk_today, dawn_tomorrow, sun_today, sun_tomorrow, moon_phase, location_timezone = nighttime(city,country,timezone,latitude,longitude)
 
-    if not sunset <= now < sunrise:
+    if sunset <= now < sunrise:
         if not measured_today:
             try:
-               measurement(IP,PORT,timeinterval,sunset,now,sunrise,city,serial)
+               measurement(IP,PORT,timeinterval,sunset,now,sunrise,dusk_today,dawn_tomorrow,city,serial,moon_phase,location_timezone)
                measured_today=True
             except Exception as e:
                 log(f"Critical measurement error: {e}")
